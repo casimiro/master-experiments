d7a7e8300ca8f29d81e6a38c3d5a19e3a3ebdb2b
Caio Casimiro <caio.casimiro@bitforge.com.br>
2013-12-25 23:23:15 -0200

    Implemented main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <QSqlDatabase>
+#include <fstream>
+#include "twitteruser.h"
+#include "evaluation.h"
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+using namespace casimiro;
+
+QDateTime START_PROFILE = QDateTime::fromString("2013-01-01 00:00:00", "yyyy-MM-dd HH:mm:ss");
+QDateTime END_PROFILE = QDateTime::fromString("2013-05-01 00:00:00", "yyyy-MM-dd HH:mm:ss");
+
+QDateTime START_RETWEETS = QDateTime::fromString("2013-05-01 00:00:01", "yyyy-MM-dd HH:mm:ss");
+QDateTime END_RETWEETS = QDateTime::fromString("2013-08-01 00:00:00", "yyyy-MM-dd HH:mm:ss");
+
+int CANDIDATE_PERIOD_IN_HOURS = 48;
+
+int main(int /*argc*/, char** /* argv*/) {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName("192.168.25.33");
+    db.setDatabaseName("tweets");
+    db.setUserName("tweets");
+    db.setPassword("zxc123");
+
+    if(!db.open())
+    {
+        std::cerr << "Could not open database" << std::endl;
+        return -1;
+    }
+    
+    Evaluation evaluation;
+    evaluation.evaluateSystem("users.csv", START_PROFILE, END_PROFILE, START_RETWEETS, END_RETWEETS, CANDIDATE_PERIOD_IN_HOURS, "results.csv");
+    
     return 0;
 }
